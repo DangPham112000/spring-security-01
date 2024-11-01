0437d73b50e95ad17d3f9007da8ef2b696928fb4
DangPham112000 <dangpham112000@gmail.com>
2024-11-01 16:47:09 +0700

    filterchain

--- src/main/java/com/dangpham112000/springsecurity01/config/SecurityConfig.java
+++ src/main/java/com/dangpham112000/springsecurity01/config/SecurityConfig.java
@@ -2,22 +2,52 @@ package com.dangpham112000.springsecurity01.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(jsr250Enabled = true)
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.formLogin((formLogin) -> formLogin.loginProcessingUrl("/login"));
+        http.authorizeRequests(req -> req
+                        .requestMatchers("/api/v1/auth/login", "/api/v1/auth/logout")
+                        .permitAll()
+//                .requestMatchers("/v1/admin/vip")
+//                .hasRole("ADMIN")
+//                .requestMatchers("/v1/admin/normal")
+//                .hasAnyRole("ADMIN", "USER")
+                        .anyRequest()
+                        .authenticated()
+        );
+
+        return http.build();
+    }
+
     // Config User Information
     @Bean
     public UserDetailsService userDetailsService() {
 
-        UserDetails admin = User.withUsername("admin").password("{noop}1234").roles("admin", "user").build();
+        UserDetails admin = User
+                .withUsername("admin")
+                .password("{noop}1234") // raw
+                .authorities("ROLE_ADMIN", "ROLE_USER")
+                .build();
 
-        UserDetails user = User.withUsername("user").password("{noop}1234").roles("user").build();
+        UserDetails user = User
+                .withUsername("user")
+                .password("{noop}1234")
+                .authorities("ROLE_USER")
+                .build();
 
         return new InMemoryUserDetailsManager(admin, user);
     }
--- src/main/java/com/dangpham112000/springsecurity01/controller/AdminController.java
+++ src/main/java/com/dangpham112000/springsecurity01/controller/AdminController.java
@@ -1,5 +1,8 @@
 package com.dangpham112000.springsecurity01.controller;
 
+import jakarta.annotation.security.RolesAllowed;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1/admin/")
 public class AdminController {
 
+    @RolesAllowed("ADMIN")
     @GetMapping("/vip")
     public String zoneVIP() {
         return "zoneVIP";
     }
 
+    @RolesAllowed({"USER", "ADMIN"})
     @GetMapping("/normal")
     public String zoneNormal() {
         return "zoneNormal";
+    }
+
+    @GetMapping("/info")
+    public Authentication getInfoUser() {
+        return SecurityContextHolder.getContext().getAuthentication();
     }
 }
